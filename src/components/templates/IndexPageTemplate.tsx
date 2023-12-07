a5f7709a451a61e2e51df9587429e2f960dc884d
kimizuka <f.kimizuka@gmail.com>
2023-12-07 16:07:14 +0900

    page router

---
+++ src/components/templates/IndexPageTemplate.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { useEffect, useRef } from 'react';
+import { usePageStateContext } from '@/contexts/PageStateContext';
+import { useResize } from '@/hooks/useResize';
+import { useScroll } from '@/hooks/useScroll';
+
+export function IndexPageTemplate() {
+  const windowRef = useRef<Window | null>(null);
+  const { windowWidth, windowHeight } = useResize();
+  const { scrollTop } = useScroll(windowRef);
+  const { isLoading, setIsLoading } = usePageStateContext();
+
+  useEffect(() => {
+    windowRef.current = window;
+  }, []);
+
+  useEffect(() => {
+    const delay = 1000;
+
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, delay);
+  }, [setIsLoading]);
+
+  return (
+    <Wrapper>
+      <p>Page Router</p>
+      <div className='console'>
+        <p>windowWidth: { windowWidth }</p>
+        <p>windowHeight: { windowHeight }</p>
+        <p>scrollTop: { scrollTop }</p>
+        <p>isLoading: { String(isLoading) }</p>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  padding: 8px;
+  height: 200vh;
+
+  .console {
+    position: fixed;
+    top: 8px; right: 8px;
+    text-align: right;
+  }
+`;
