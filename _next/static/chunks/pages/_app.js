e260c87afe1b13ff78f5a0bdc428bfbd7219b71a
kimizuka <kimizuka@users.noreply.github.com>
2025-08-29 08:48:58 +0000

    deploy: 2c0edd45b4cb09cf44759632ad30ed3f6718cff0

--- _next/static/chunks/pages/_app.js
+++ _next/static/chunks/pages/_app.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/440e2f6697529b45.js",
-  "static/chunks/6ff498b3e8dca7cf.js",
-  "static/chunks/e89476a22e6e789a.js"
+  "static/chunks/cd4fef68186195ea.js",
+  "static/chunks/710d2471b5bde11f.js"
 ])
--- _next/static/chunks/pages/_error.js
+++ _next/static/chunks/pages/_error.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_error", [
   "static/chunks/b52587224839defe.js",
-  "static/chunks/6ff498b3e8dca7cf.js",
-  "static/chunks/0c4b39c5d5c7b1e4.js"
+  "static/chunks/cd4fef68186195ea.js",
+  "static/chunks/c61676d0d18fef95.js"
 ])
