529d8ee412109b6b510ffa989d5b5d6fe87a738a
kimizuka <f.kimizuka@gmail.com>
2025-08-29 17:48:00 +0900

    fixed asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from 'next';
 
+const repositoryName: string = 'nextjs';
+const isProduction: boolean = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: 'export',
+  ...(isProduction
+    ? {
+        basePath: `/${repositoryName}`,
+        assetPrefix: `/${repositoryName}/`,
+      }
+    : {}),
 };
 
 export default nextConfig;
