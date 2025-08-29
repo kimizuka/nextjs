ad4411a1f002327b3ef8b62be43d3d92432edae4
kimizuka <f.kimizuka@gmail.com>
2025-08-29 16:40:03 +0900

    add storybook

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,44 @@
+import styles from './index.module.css';
+
+export interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const buttonSize = (() => {
+    switch (size) {
+      case 'small':
+        return styles.buttonSmall;
+      case 'large':
+        return styles.buttonLarge;
+      default:
+        return styles.buttonMedium;
+    }
+  })();
+  const buttonMode = primary ? styles.buttonPrimary : styles.buttonSecondary;
+
+  return (
+    <button
+      type="button"
+      className={[styles.button, buttonSize, buttonMode].join(' ')}
+      {...props}
+    >
+      {label}
+      <style jsx>{`
+        button {
+          background-color: ${backgroundColor};
+        }
+      `}</style>
+    </button>
+  );
+};
