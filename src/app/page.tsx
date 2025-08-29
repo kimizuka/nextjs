21bf6f7625e1f709b20547de3b56f8661ab2ffb3
kimizuka <f.kimizuka@gmail.com>
2025-08-29 19:32:20 +0900

    add secret key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import styles from './index.module.css';
 
+if (!process.env.SECRET_KEY) {
+  throw new Error('SECRET_KEY is not defined in environment variables.');
+}
+
 export default function IndexPage() {
   return (
     <div className={styles.container}>{process.env.NEXT_PUBLIC_MESSAGE}</div>
